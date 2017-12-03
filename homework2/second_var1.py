2f5e0873cfa84b4d1ed4bf349e434515b26f446e
k-koleda <32541582+k-koleda@users.noreply.github.com>
2017-12-03 13:50:39 +0200

    Update second_var1.py

--- homework2/second_var1.py
+++ homework2/second_var1.py
@@ -1,1 +1,16 @@
-
+list1 = [1,2,3,4,5,6,7]
+list2 = [1,2,3,4,2,3]
+pos = 0
+dic = {}
+if len(list1) < len(list2):
+    for _ in list1:
+        dic[_] = list2[pos]
+        pos += 1
+else:
+    for _ in list1:
+        if pos < len(list2):
+            dic[_] = list2[pos]
+            pos += 1
+        else:
+            dic[_] = None
+print dic

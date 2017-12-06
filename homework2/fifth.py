2dfcaee792cfe95e4f4bc45fc023273273af03e3
k-koleda <32541582+k-koleda@users.noreply.github.com>
2017-12-06 08:04:03 +0200

    Update fifth.py
    corrected your remark

--- homework2/fifth.py
+++ homework2/fifth.py
@@ -1,11 +1,13 @@
 output=[]
 input=[]
-for _ in range(1,int(raw_input())):
+for _ in range(int(raw_input())):
     input.append(_)
 for j in input:
-    if j % 5 == 0 and j % 3 == 0:
+    if j == 0:
+        output.append(j)
+    elif j % 5 == 0 and j % 3 == 0:
         output.append("fizbuzz")
-    if j % 3 == 0:
+    elif j % 3 == 0:
         output.append("fiz")
     elif j % 5 == 0:
         output.append("buzz")
